a14d2ced5769932316a00291f9867efb71d68ace
Michele Oliveira <shellyguns@gmail.com>
2026-03-13 23:37:43 -0300

    feat: inicialização do setup - primeiro commit

---
+++ src/main/java/br/com/banco/gestao_contas/GestaoContasApplication.java
@@ -0,0 +1,13 @@
+package br.com.banco.gestao_contas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoContasApplication.class, args);
+	}
+
+}
